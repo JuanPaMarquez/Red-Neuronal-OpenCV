c2bdc1766cb9fc5b5c7ea2db5bcee97e20483116
JuanPaMarquez <juan013sanchez@gmail.com>
2024-06-09 22:30:52 -0500

    pdf Ejemplo

---
+++ connection/diccionario.py
@@ -0,0 +1,10 @@
+usuarios = {
+    'usuario1': {'edad': 20, 'email': 'usuario1@example.com', 'ciudad': 'Bogotá'},
+    'usuario2': {'edad': 25, 'email': 'usuario2@example.com', 'ciudad': 'Medellín'},
+    'usuario3': {'edad': 30, 'email': 'usuario3@example.com', 'ciudad': 'Cali'}
+}
+
+# Agregar un nuevo usuario
+usuarios['usuario4'] = {'edad': 35, 'email': 'usuario4@example.com', 'ciudad': 'Cartagena'}
+
+print(usuarios)
